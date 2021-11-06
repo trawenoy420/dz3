ac5f37de5aae823d34b86e62abdcca660ab2a078
trawenoy420 <92788214+trawenoy420@users.noreply.github.com>
2021-11-06 15:01:10 +0300

    исправил

--- p1.py
+++ p1.py
@@ -1,4 +1,9 @@
-def first_func(var_1, var_2, var_3):
-    return var_1 + var_2 + var_3
-
-print(first_func(10, 20, 30))
+def my_func (x, y):
+    try:
+        z = x / y
+        return z
+    except ZeroDivisionError:
+        return "y is'n be a zero"
+    except ValueError:
+        return "enter only number"
+print(my_func(int(input("Enter x = ")), int(input("Enter y = "))))
